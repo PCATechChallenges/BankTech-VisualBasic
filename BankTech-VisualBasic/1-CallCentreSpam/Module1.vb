8f7f1f1cab75b1046d2d034068d537aff5dac7b6
derekh <derek.hampton@augmensis.co.uk>
2015-04-23 12:07:09 +0100

    Added Call Centre Spam solution

--- BankTech-VisualBasic/1-CallCentreSpam/Module1.vb
+++ BankTech-VisualBasic/1-CallCentreSpam/Module1.vb
@@ -1,6 +1,31 @@
-﻿Module Module1
+﻿Imports System.IO
+
+Module Module1
 
     Sub Main()
+
+        Dim phoneList = "PhoneList.txt"
+        Dim numbers = File.ReadLines(phoneList)
+        Dim spamCount = 0
+        Dim goodCount = 0
+
+        For Each phoneNumber As String In numbers
+
+            Dim isSpam = phoneNumber.Contains("SPAM")
+
+            If isSpam Then
+                spamCount += 1
+            Else
+                goodCount += 1
+            End If
+
+            Console.WriteLine(If(isSpam, "SPAM", "OK"))
+
+        Next
+
+        Console.WriteLine(vbCrLf + "{2} numbers processed" + vbCrLf + "{0} SPAM numbers found" + vbCrLf + "{1} numbers were OK", spamCount, goodCount, spamCount + goodCount)
+        Console.WriteLine("Press any key to exit...")
+        Console.ReadLine()
 
     End Sub
 
--- BankTech-VisualBasic/6-WebScrapingSpiderBot/Module1.vb
+++ BankTech-VisualBasic/6-WebScrapingSpiderBot/Module1.vb
@@ -1,5 +1,13 @@
 ﻿Module Module1
 
+    ' ## Extra challenges ##
+    ' 1. What would you do to enable this to output wordcounts by each set of links followed?
+    ' 2. The word count currently takes ALL text from an html file. What would you do to to only focus on the visible on-page text?
+    ' 3. The URL handling currently needs full http:// addresses to follow links. How could you do this differently to make it more robust?
+
+    ' DISCLAIMER: Remember, not all sites like to be scraped by robots. In some places it's probably even illegal. 
+    ' ALWAYS MAKE SURE YOU HAVE THE SITE OWNER'S PERMISSION BEFORE GOING OUT AND SCRAPING STUFF!!
+
     Sub Main()
 
     End Sub
