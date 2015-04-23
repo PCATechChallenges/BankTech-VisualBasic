cb1e95fdc73a3655984325340ffe4130edb32148
derekh <derek.hampton@augmensis.co.uk>
2015-04-23 12:22:09 +0100

    Added Keeping Track of Criminals solution

--- BankTech-VisualBasic/2-KeepingTrackOfCriminals/Module1.vb
+++ BankTech-VisualBasic/2-KeepingTrackOfCriminals/Module1.vb
@@ -2,6 +2,28 @@
 
     Sub Main()
 
+        Dim serialNumber = ""
+        While serialNumber = ""
+            Console.WriteLine("Please input the serial number to print:")
+            serialNumber = Console.ReadLine()
+        End While
+
+
+        Dim timesToPrint = 0
+        While timesToPrint = 0
+            Console.WriteLine("Please enter the number of times to print {0}:", serialNumber)
+            Integer.TryParse(Console.ReadLine(), timesToPrint)
+            If timesToPrint = 0 Then Console.WriteLine("That was an invalid number. Please try again.")
+        End While
+
+        'Print out the results!
+        For i As Integer = 1 To timesToPrint
+            Console.WriteLine(vbTab + serialNumber)
+        Next
+
+        Console.WriteLine("DONE! Printed {0} {1} times", serialNumber, timesToPrint)
+        Console.ReadLine()
+
     End Sub
 
 End Module
